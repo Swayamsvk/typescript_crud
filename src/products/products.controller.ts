820ec0498c21709b9a51077bd4643aea0978196f
Swayam Samyak Debasis <you@example.com>
2020-08-04 17:13:25 +0530

    created project

---
+++ src/products/products.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+//filter for requests that starts with /products
+@Controller('products')
+export class ProductController {
+  constructor(readonly productService: ProductsService) {}
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+  @Get()
+  getAllProducts() {
+    return this.productService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productService.deleteProduct(prodId);
+    return null;
+  }
+}
